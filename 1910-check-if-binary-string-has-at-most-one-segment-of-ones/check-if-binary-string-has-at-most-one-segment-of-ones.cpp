d49036820c2f3a6b41e7ec384d028731eda3d505
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-03-06 17:27:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.47%) - LeetSync

---
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        bool zeroSeen = false;
+
+        for(char c : s){
+            if(c == '0'){
+                zeroSeen = true;
+            }
+            else if(zeroSeen){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
