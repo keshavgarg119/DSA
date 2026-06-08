99ae43492b45e3153588bfa6c24470966a4d3c60
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-08 11:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (85.80%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        if(x == 0 || x == 1) return x;
+        
+        int low = 0;
+        int high = x/2;
+
+        while(low<=high) {
+            long long mid = low + (high - low)/2;
+
+            if(mid*mid == x) {
+                return mid;
+            }
+            else if(mid*mid > x) {
+                high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+        }
+
+        return high;
+    }
+};
