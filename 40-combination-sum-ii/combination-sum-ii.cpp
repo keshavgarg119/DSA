6c85a6b569f6d6c76461940c917583718d05a3c8
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-09 22:03:00 +0530

    Time: 1 ms (49.12%) | Memory: 14 MB (82.60%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-
-    void findCombination(int index, int target, vector<int>& arr, vector<vector<int>>& ans, vector<int>& ds) {
-
-        // base case
-        if(target == 0) {
-            ans.push_back(ds);
+    void findCombinations(int index, vector<int>&candidates, int target, vector<int>& subset, vector<vector<int>>& ans) {
+        if(target==0) {
+            ans.push_back(subset);
             return;
         }
 
-        // checking for all the other combination without duplicates
-        for(int i=index; i<arr.size(); i++) {
-            
-            if(i>index && arr[i] == arr[i-1])  continue;// checking for duplicates
-            if(arr[i] > target) break; // checking whether any number less than target still present
-            
-            ds.push_back(arr[i]);
-            findCombination(i+1, target-arr[i], arr, ans, ds);
-            ds.pop_back();
+        for(int i = index; i<candidates.size(); i++) {
+            if(i!=index && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) break;
 
+            subset.push_back(candidates[i]);
+            findCombinations(i+1, candidates, target-candidates[i], subset, ans);
+            subset.pop_back();
         }
-
+        
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>subset;
 
         sort(candidates.begin(), candidates.end());
-        vector<vector<int>> ans;
-        vector<int>ds;
-        findCombination(0,target,candidates,ans,ds);
-        return ans;
+        findCombinations(0, candidates, target, subset, ans);
 
+        return ans;
     }
 };
