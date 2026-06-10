54e667dfe95570776ba5026c29a99aff50a297e9
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-10 09:55:10 +0530

    Time: 30 ms (88.26%) | Memory: 53.3 MB (92.04%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
-   
     bool isPalindrome(string &s, int start, int end) {
         while(start<=end) {
             if(s[start]!=s[end]) {
                 return false;
             }
-            start++,end--;
+            start++;
+            end--;
         }
-        return true;
+        return true;;
     }
 
-    void func(int index, string &s,vector<string> &path, vector<vector<string>> &res) {
-
-        if(index == s.size()) {
-            res.push_back(path);
+    void palindromePart(int index, string &s, vector<string>& subset, vector<vector<string>>& ans) {
+        if(index==s.size()) {
+            ans.push_back(subset);
             return;
         }
 
-        for(int i = index; i<s.size(); i++) {
-            if(isPalindrome(s,index,i)) {
-                path.push_back(s.substr(index, i-index+1));
-                func(i+1,s,path,res);
-                path.pop_back();
+        for(int i=index; i<s.size(); i++) {
+            if(isPalindrome(s, index, i)) {
+                subset.push_back(s.substr(index, i-index+1));
+                palindromePart(i+1, s, subset, ans);
+                subset.pop_back();
             }
         }
-
     }
-
-
     vector<vector<string>> partition(string s) {
-        vector<string> path;
-        vector<vector<string>> res;
-        func(0,s,path,res);
-        return res;
+        vector<string>subset;
+        vector<vector<string>>ans;
+
+        palindromePart(0, s, subset, ans);
+
+        return ans;
     }
 };
