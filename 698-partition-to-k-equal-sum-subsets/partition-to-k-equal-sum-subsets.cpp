9ee703d51acbdd8e4fa827d84609e06e5b6f2cf7
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-10 18:48:48 +0530

    Time: 4 ms (66.62%) | Memory: 12 MB (75.89%) - LeetSync

---
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool solve(int index, int target, vector<int>& nums, vector<int>& bucket) {
+        if(index==nums.size()) {
+            return true;
+        }
+
+        for(int i=0; i<bucket.size(); i++) { // remember, we are treing for different different baskets here
+            if(bucket[i]+nums[index]>target) {
+                continue;
+            }
+
+            bucket[i] += nums[index];
+            if(solve(index+1, target, nums, bucket)) {
+                return true;
+            };
+            bucket[i] -= nums[index];
+
+            if(bucket[i]==0) break;
+        }
+
+        return false;
+    }
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int target = 0;
+        int totalsum = 0;
+
+        for(int i=0; i<nums.size(); i++) {
+            totalsum += nums[i];
+        }
+
+        if(totalsum%k!=0) return false; // partition not possible;
+
+        target = totalsum/k;
+        sort(nums.rbegin(), nums.rend());
+
+        if(nums[0]>target) return false; //largest element is greater than target
+
+        vector<int>bucket(k, 0);
+
+        return solve(0, target, nums, bucket);
+    }
+};
