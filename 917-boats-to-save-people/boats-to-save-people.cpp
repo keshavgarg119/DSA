89343ca441ec0436b92e6095a313c45f4ad11079
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-11 21:03:22 +0530

    Time: 20 ms (37.38%) | Memory: 45.7 MB (96.60%) - LeetSync

---
+++ 917-boats-to-save-people/boats-to-save-people.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int ans = 0;
+
+        sort(people.begin(), people.end());
+        int left = 0;
+        int right = people.size()-1;
+
+        while(left<=right) {
+            if(people[left]+people[right]<=limit) {
+                left++;
+            }
+
+            right--;
+            ans++;
+        }
+        return ans;
+    }
+};
