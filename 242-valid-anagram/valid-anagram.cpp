3689ce4066de76d92d47945adcdbdf9ae0fc7d16
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-12 16:58:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+
+        // this is the solution, if input doent contains unicode
+
         if(s.size()!=t.size()) return false;
 
-        vector<int>freq(26,0);
+        // vector<int>freq(26,0);
+
+        // for(char ch: s) {
+        //     freq[ch-'a']++;
+        // }
+
+        // for(char ch: t) {
+        //     freq[ch-'a']--;
+        // }   
+
+        // for(int count: freq) {
+        //     if(count > 0) return false;
+        // }
+
+        // return true;
+//..........................................................................
+        
+        // if unicodes are also contained
+
+        unordered_map<int,int>mp;
 
         for(char ch: s) {
-            freq[ch-'a']++;
+            mp[ch]++;
         }
-
         for(char ch: t) {
-            freq[ch-'a']--;
-        }   
+            mp[ch]--;
 
-        for(int count: freq) {
-            if(count > 0) return false;
+            if(mp[ch]<0) return false;
         }
 
         return true;
+
 
     }
 };
