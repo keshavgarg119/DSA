66c57e335f083d5ca45ab7fcebbed46c53104766
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-14 17:45:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (54.94%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+        unordered_map<char,int>mp;
+
+        for(char ch: s) {
+            mp[ch]++;
+        }
+
+        priority_queue<pair<int,char>>pq;
+
+        for(auto it: mp) {
+            if(it.second > (s.size()+1)/2) {
+                return "";
+            }
+
+            pq.push({it.second, it.first});
+        }
+
+        string ans = "";
+        while(pq.size()>=2) {
+            auto first = pq.top();
+            pq.pop();
+
+            auto second = pq.top();
+            pq.pop();
+
+            char ch1 = first.second;
+            int freq1 = first.first;
+
+            char ch2 = second.second;
+            int freq2 = second.first;
+
+            ans += ch1;
+            freq1--;
+
+            ans += ch2;
+            freq2--;
+
+            if(freq1>0) {
+                pq.push({freq1, ch1});
+            }
+            if(freq2>0) {
+                pq.push({freq2, ch2});
+            }
+        }
+
+        if(!pq.empty()) {
+            ans += pq.top().second;
+        }
+
+        return ans;
+    }
+};
