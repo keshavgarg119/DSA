dfa489b764fd65d3e86ee84cfa93dda3fb3aa385
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-17 11:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (68.42%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -10,18 +10,22 @@ public:
     // }
     int tribonacci(int n) {
 
-        vector<int>dp(n+1, 0);
+        //vector<int>dp(n+1, 0);
 
         if(n==0) return 0;
         if(n==1 || n==2) return 1;
 
-        dp[0] = 0;
-        dp[1] = dp[2] = 1;
+        int prev3 = 0;
+        int prev1 = 1;
+        int prev2 = 1;
 
         for(int i=3; i<=n; i++) {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+            int curr = prev1 + prev2 + prev3;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
 
-        return dp[n];
+        return prev1;
     }
 };
