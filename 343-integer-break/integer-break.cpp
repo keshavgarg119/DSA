60dd9872a9cbd7323441f4278030950b8139eb0e
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-19 19:13:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (20.47%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -13,8 +13,40 @@ public:
         return dp[n] = ans;
     }
     int integerBreak(int n) {
-        vector<int>dp(n+1,-1);
+        vector<int>dp(n+1,0);
 
-        return solve(n,dp);
+        // if(n==1) return 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n; i++) {
+            for(int j=1; j<i; j++) {
+                dp[i] = max(dp[i], max(j*(i-j), j*dp[i-j]));
+            }
+        }
+
+        return dp[n];
     }
 };
+
+
+
+
+// key idea
+
+// 1|9      max(1*9, 1*dp[9])
+
+// 2|8      max(2*8, 2*dp[8])
+
+// 3|7      max(3*7, 3*dp[7])
+
+// 4|6      max(4*6, 4*dp[6])
+
+// 5|5      max(5*5, 5*dp[5])
+
+// 6|4      max(6*4, 6*dp[4])
+
+// 7|3      max(7*3, 7*dp[3])
+
+// 8|2      max(8*2, 8*dp[2])
+
+// 9|1      max(9*1, 9*dp[1])
