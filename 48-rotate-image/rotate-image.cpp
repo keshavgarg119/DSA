af9acdfbb15c865d1dac7979cede59c6fc5684b0
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-21 16:21:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (91.92%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,14 +3,25 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        vector<vector<int>> ans(n, vector<int>(n));
+        // vector<vector<int>> ans(n, vector<int>(n));
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                ans[j][n-1-i] = matrix[i][j];
+        // for(int i=0; i<n; i++) {
+        //     for(int j=0; j<n; j++) {
+        //         ans[j][n-1-i] = matrix[i][j];
+        //     }
+        // }
+
+        // matrix = ans;
+
+        for(int i=0; i<=n-2;i++) {
+            for(int j=i+1; j<=n-1;j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
+        
+        for(int i=0; i<n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
 
-        matrix = ans;
     }
 };
