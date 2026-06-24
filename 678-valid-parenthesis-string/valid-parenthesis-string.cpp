247624c3c216142ad0485d642d79cd965da9d4c0
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-24 17:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (26.10%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,36 +1,59 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        stack<char>open;
-        stack<char>star;
+        // stack<char>open;
+        // stack<char>star;
 
-        for(int i=0; i<s.size(); i++) {
-            if(s[i]=='(') {
-                open.push(i);
+        // for(int i=0; i<s.size(); i++) {
+        //     if(s[i]=='(') {
+        //         open.push(i);
+        //     }
+        //     else if(s[i]=='*') {
+        //         star.push(i);
+        //     }
+        //     else {
+        //         if(!open.empty()) {
+        //             open.pop();
+        //         }
+        //         else if(!star.empty()) {
+        //             star.pop();
+        //         }
+        //         else {
+        //             return false;
+        //         }
+        //     }
+        // }
+
+        // while(!open.empty() && !star.empty()) {
+        //     if(open.top() > star.top()) return false;
+
+        //     open.pop();
+        //     star.pop();
+        // }
+
+        // return open.empty();
+
+        int low = 0, high = 0;
+
+        for(char ch: s) {
+            if(ch=='(') {
+                low++;
+                high++;
             }
-            else if(s[i]=='*') {
-                star.push(i);
+            else if(ch==')') {
+                low--;
+                high--;
             }
             else {
-                if(!open.empty()) {
-                    open.pop();
-                }
-                else if(!star.empty()) {
-                    star.pop();
-                }
-                else {
-                    return false;
-                }
+                low--;
+                high++;
             }
+
+            if(high<0) return false;
+
+            low = max(low,0);
         }
 
-        while(!open.empty() && !star.empty()) {
-            if(open.top() > star.top()) return false;
-
-            open.pop();
-            star.pop();
-        }
-
-        return open.empty();
+        return low==0;
     }
 };
