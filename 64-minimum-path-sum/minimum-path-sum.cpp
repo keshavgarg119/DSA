390143bbb65b19ebe0c7e6493f01f0d30632a3a5
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-26 17:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (72.60%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -15,24 +15,28 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>>dp(m, vector<int>(n,0));
+        // vector<vector<int>>dp(m, vector<int>(n,0));
+        vector<int>prev(n,0);
 
         for(int i=0; i<m; i++) {
+            vector<int>curr(n,0);
+
             for(int j=0; j<n; j++) {
                 if(i==0 && j==0){
-                    dp[i][j] = grid[0][0];
+                    curr[j] = grid[0][0];
                     continue;
                 }
 
                 int up = 1e9, left = 1e9;
                 
-                if(i>0) up = dp[i-1][j];
-                if(j>0) left = dp[i][j-1];
+                if(i>0) up = prev[j];
+                if(j>0) left = curr[j-1];
 
-                dp[i][j] = grid[i][j] + min(up, left);
+                curr[j] = grid[i][j] + min(up, left);
             }
+            prev = curr;
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
