9453efb89422830f352047f446287b3006d6b316
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-26 10:52:37 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (6.21%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -18,8 +18,34 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        vector<vector<int>>dp(m, vector<int>(n,-1));
+        vector<vector<int>>dp(m, vector<int>(n,0));
 
-        return solve(m-1, n-1, dp, obstacleGrid);
+        if(obstacleGrid[0][0] == 1) return 0;
+
+        dp[0][0] = 1;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(i==0 && j==0) continue;
+                
+                if(obstacleGrid[i][j]==1){ 
+                    dp[i][j] = 0;
+                    continue;
+                }
+                
+                int up = 0;
+                int left = 0;
+
+                if(i > 0)
+                    up = dp[i-1][j];
+
+                if(j > 0)
+                    left = dp[i][j-1];
+
+                dp[i][j] = up + left;
+            }
+        }
+
+        return dp[m-1][n-1];
     }
 };
