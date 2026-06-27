034a5cbc03c7350af7edc780668de45bf7c4967e
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-27 09:40:04 +0530

    Time: 3 ms (85.57%) | Memory: 15.5 MB (48.36%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int solve(int i, int sum, vector<int>& nums, vector<vector<int>>& dp) {
-        if(i==0) {
-            if(sum==0 && nums[0]==0) return 2;
+    // int solve(int i, int sum, vector<int>& nums, vector<vector<int>>& dp) {
+    //     if(i==0) {
+    //         if(sum==0 && nums[0]==0) return 2;
 
-            if(sum==0 || sum==nums[0]) {
-                return 1;
-            }
+    //         if(sum==0 || sum==nums[0]) {
+    //             return 1;
+    //         }
 
-            return 0;
-        }
+    //         return 0;
+    //     }
 
-        if(dp[i][sum]!=-1) return dp[i][sum];
+    //     if(dp[i][sum]!=-1) return dp[i][sum];
 
-        int notTake = solve(i-1, sum, nums, dp);
+    //     int notTake = solve(i-1, sum, nums, dp);
 
-        int take = 0;
-        if(nums[i] <= sum) {
-            take = solve(i-1, sum-nums[i], nums, dp);
-        }
+    //     int take = 0;
+    //     if(nums[i] <= sum) {
+    //         take = solve(i-1, sum-nums[i], nums, dp);
+    //     }
 
-        return dp[i][sum] = take + notTake;
-    }
+    //     return dp[i][sum] = take + notTake;
+    // }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
         int total = accumulate(nums.begin(), nums.end(), 0);
@@ -31,8 +31,29 @@ public:
 
         int req = (total+target)/2;
 
-        vector<vector<int>>dp(n, vector<int>(req+1,-1));
+        vector<vector<int>>dp(n, vector<int>(req+1,0));
 
-        return solve(n-1, req, nums, dp);
+        if (nums[0] == 0)
+            dp[0][0] = 2;
+        else
+            dp[0][0] = 1;
+
+        if (nums[0] != 0 && nums[0] <= req)
+            dp[0][nums[0]] = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int sum = 0; sum <= req; sum++) {
+
+                int notTake = dp[i - 1][sum];
+
+                int take = 0;
+                if (nums[i] <= sum)
+                    take = dp[i - 1][sum - nums[i]];
+
+                dp[i][sum] = take + notTake;
+            }
+        }
+
+        return dp[n - 1][req];
     }
 };
