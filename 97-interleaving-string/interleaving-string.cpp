f307ee20a9676f47b506a321d2228d36ce8f37aa
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-27 17:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (65.17%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,30 +1,51 @@
 class Solution {
 public:
-    bool solve(int i, int j, string &s1, string &s2, string &s3, vector<vector<int>>& dp) {
-        if(i==s1.size() && j==s2.size()) return true;
+    // bool solve(int i, int j, string &s1, string &s2, string &s3, vector<vector<int>>& dp) {
+    //     if(i==s1.size() && j==s2.size()) return true;
 
-        if(dp[i][j]!=-1) return dp[i][j]; // i characters from s1 and j characters from s2
+    //     if(dp[i][j]!=-1) return dp[i][j]; // i characters from s1 and j characters from s2
 
-        bool takeFromS1 = false, takeFromS2 = false;
+    //     bool takeFromS1 = false, takeFromS2 = false;
 
-        if(i<s1.size() && s1[i]==s3[i+j]) {
-            takeFromS1 = solve(i+1, j, s1,s2,s3, dp);
-        }
+    //     if(i<s1.size() && s1[i]==s3[i+j]) {
+    //         takeFromS1 = solve(i+1, j, s1,s2,s3, dp);
+    //     }
 
-        if(j<s2.size() && s2[j]==s3[i+j]) {
-            takeFromS2 = solve(i, j+1, s1,s2,s3, dp);
-        }
+    //     if(j<s2.size() && s2[j]==s3[i+j]) {
+    //         takeFromS2 = solve(i, j+1, s1,s2,s3, dp);
+    //     }
 
-        return dp[i][j] = takeFromS1 || takeFromS2;
-    }
+    //     return dp[i][j] = takeFromS1 || takeFromS2;
+    // }
+    
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.size();
         int m = s2.size();
         int nm = n + m;
 
         if(nm != s3.size()) return false;
-        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+        // vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+        vector<vector<bool>>dp(n+1, vector<bool>(m+1, false));
 
-        return solve(0,0,s1,s2,s3,dp);
+        dp[n][m] = true;
+        for(int i=n; i>=0; i--) {
+            for(int j=m; j>=0; j--) {
+
+                if(i==n && j==m) continue;
+
+                bool takeFromS1 = false, takeFromS2 = false;
+
+                if(i<s1.size() && s1[i]==s3[i+j]) {
+                    takeFromS1 = dp[i+1][j];
+                }
+                if(j<s2.size() && s2[j]==s3[i+j]) {
+                    takeFromS2 = dp[i][j+1];
+                }
+
+                dp[i][j] = takeFromS1 || takeFromS2;
+            }
+        }
+
+        return dp[0][0];
     }
 };
