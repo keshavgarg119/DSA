3c6d76948efa0a288c7942743aca602f78dff69a
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-27 10:02:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ways to Tile the Floor/ways-to-tile-the-floor.cpp
+++ Difficulty: Medium/Ways to Tile the Floor/ways-to-tile-the-floor.cpp
@@ -2,20 +2,32 @@ class Solution {
   public:
     const int MOD = 1e9 + 7;
     
-    int solve(int n, int m, vector<int>& dp) {
-        if(n<m) return 1;
+    // int solve(int n, int m, vector<int>& dp) {
+    //     if(n<m) return 1;
         
-        if(n==m) return 2;
+    //     if(n==m) return 2;
         
-        if(dp[n]!=-1) return dp[n];
+    //     if(dp[n]!=-1) return dp[n];
         
-        return dp[n] = (solve(n-1,m,dp) + solve(n-m,m,dp)) % MOD;
-    }
+    //     return dp[n] = (solve(n-1,m,dp) + solve(n-m,m,dp)) % MOD;
+    // }
     
     int countWays(int n, int m) {
         // code here.
-        vector<int>dp(n+1, -1);
+        vector<int>dp(n+1, 0);
         
-        return solve(n,m,dp);
+        dp[0] = 1;
+        for(int i=1; i<=n; i++) {
+            if(i<m) {
+                dp[i] = 1;
+            }
+            else if(i==m){ 
+                dp[i] = 2;
+            }
+            else {
+                dp[i] = (dp[i-1] + dp[i-m]) % MOD;
+            }
+        }
+        return dp[n];
     }
 };
