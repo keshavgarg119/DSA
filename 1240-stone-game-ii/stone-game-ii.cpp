eef1e7d72695efe4aeb3dd71b4b65f06f0fdbc7e
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-28 17:47:33 +0530

    Time: 35 ms (23.86%) | Memory: 13.7 MB (47.05%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int solve(int i, int m, vector<int>& suffix, vector<vector<int>>& dp) {
-        int n = suffix.size();
-        if(i>=n) return 0;
+    // int solve(int i, int m, vector<int>& suffix, vector<vector<int>>& dp) {
+    //     int n = suffix.size();
+    //     if(i>=n) return 0;
 
-        if(2*m >= n-i) return suffix[i];
+    //     if(2*m >= n-i) return suffix[i];
 
-        if(dp[i][m]!=-1) return dp[i][m];
+    //     if(dp[i][m]!=-1) return dp[i][m];
 
-        int ans = 0;
-        for(int x=1; x<=2*m; x++) {
-            int opponent = solve(i+x, max(x,m), suffix, dp);
+    //     int ans = 0;
+    //     for(int x=1; x<=2*m; x++) {
+    //         int opponent = solve(i+x, max(x,m), suffix, dp);
 
-            ans = max(ans, suffix[i]-opponent);
-        } 
+    //         ans = max(ans, suffix[i]-opponent);
+    //     } 
 
-        return dp[i][m] = ans;
-    }
+    //     return dp[i][m] = ans;
+    // }
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
 
@@ -27,8 +27,26 @@ public:
             suffix[i] = suffix[i+1] + piles[i];
         }
 
-        vector<vector<int>>dp(n, vector<int>(n+1,-1));
+        vector<vector<int>>dp(n+1, vector<int>(n+1,0));
 
-        return solve(0,1,suffix,dp);
+        for(int i = n-1; i>=0; i--) {
+            for(int m = n; m>=1; m--) {
+                if(2-m>=n-i) {
+                    dp[i][m] = suffix[i];
+                    continue;
+                }
+
+                int ans = 0;
+                for(int x=1; x<=min(2*m, n-i); x++) {
+                    int opponent = dp[i+x][max(m,x)];
+
+                    ans = max(ans, suffix[i]-opponent);
+                } 
+
+                dp[i][m] = ans;
+            }
+        }
+
+        return dp[0][1];
     }
 };
