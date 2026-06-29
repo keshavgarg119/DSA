4bb78204b491fedbacfbbf2ab1984a2b3e3b22b0
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-06-29 09:49:17 +0530

    Time: 15 ms (82.09%) | Memory: 8.9 MB (91.20%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -16,23 +16,24 @@ public:
         int n = s.size();
         int m = t.size();
 
-        vector<vector<unsigned long long>>dp(n+1, vector<unsigned long long>(m+1,0));
+        // vector<vector<unsigned long long>>dp(n+1, vector<unsigned long long>(m+1,0));
+        vector<unsigned long long>curr(m+1,0), prev(m+1,0);
 
-        for(int i=0; i<=n; i++) {
-            dp[i][0] = 1;
-        }
+        curr[0] = 1;
+        prev[0] = 1;
 
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=m; j++) {
                 if(s[i-1]==t[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    curr[j] = prev[j-1] + prev[j];
                 }
                 else {
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
                 }
             }
+            prev = curr;
         }
 
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
