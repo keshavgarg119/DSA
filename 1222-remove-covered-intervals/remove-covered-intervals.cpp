7273e3cf85f7f75ccc8bc3f40a95e67d1bcc56d1
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-07-06 11:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (30.60%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -1,29 +1,54 @@
+// class Solution {
+// public:
+//     int removeCoveredIntervals(vector<vector<int>>& intervals) {
+//         sort(intervals.begin(), intervals.end());
+
+//         int n = intervals.size();
+//         int start = intervals[0][0];
+//         int end = intervals[0][1];
+
+//         int ans = n;
+
+//         for(int i=1; i<n; i++) {
+//             if(intervals[i][0] > start && intervals[i][1] > end) {
+//                 start = intervals[i][0];
+//                 end = intervals[i][1];
+//                 continue;
+//             }
+//             else if(intervals[i][0] >= start && intervals[i][1] <= end) {
+//                 ans--;
+//             }
+//             else if(intervals[i][0] == start && intervals[i][1] >= end) {
+//                 ans--;
+//                 end = intervals[i][1];
+//             }
+//             else if(intervals[i][0] <= start && intervals[i][1] == end) {
+//                 ans--;
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
 
-        int n = intervals.size();
-        int start = intervals[0][0];
+        sort(intervals.begin(), intervals.end(),
+        [](vector<int>& a, vector<int>& b){
+            if(a[0] == b[0])
+                return a[1] > b[1];
+            return a[0] < b[0];
+        });
+
+        int ans = 1;
         int end = intervals[0][1];
 
-        int ans = n;
+        for(int i = 1; i < intervals.size(); i++) {
 
-        for(int i=1; i<n; i++) {
-            if(intervals[i][0] > start && intervals[i][1] > end) {
-                start = intervals[i][0];
+            if(intervals[i][1] > end) {
+                ans++;
                 end = intervals[i][1];
-                continue;
-            }
-            else if(intervals[i][0] >= start && intervals[i][1] <= end) {
-                ans--;
-            }
-            else if(intervals[i][0] == start && intervals[i][1] >= end) {
-                ans--;
-                end = intervals[i][1];
-            }
-            else if(intervals[i][0] <= start && intervals[i][1] == end) {
-                ans--;
             }
         }
 
