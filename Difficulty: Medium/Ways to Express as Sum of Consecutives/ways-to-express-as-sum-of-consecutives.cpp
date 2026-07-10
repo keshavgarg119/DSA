430082013199d7ebe1f59e3180d7a802a857e4eb
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-07-10 17:02:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ways to Express as Sum of Consecutives/ways-to-express-as-sum-of-consecutives.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    int getCount(int n) {
+        // code here
+        int left = 1;
+        int right = 1;
+        
+        int sum = 1;
+        int ans = 0;
+        
+        while(left <= n/2) {
+            if(sum<n) {
+                right++;
+                sum += right;
+            }
+            else if(sum>n) {
+                sum -= left;
+                left++;
+            }
+            else {
+                ans++;
+                
+                sum-=left;
+                left++;
+            }
+        }
+        
+        return ans;
+    }
+};
