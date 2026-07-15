77458075d1794760780334c9ea13502753aa5dbb
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-07-15 09:33:57 +0530

    Time: 1 ms (21.15%) | Memory: 8.4 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumEven = 0, sumOdd = 0;
-
-        sumEven = n*(n+1);
-        sumOdd = n*n;
-
-        return gcd(sumEven, sumOdd);
+        
+        return gcd(n*(n+1), n*n);
     }
 };
