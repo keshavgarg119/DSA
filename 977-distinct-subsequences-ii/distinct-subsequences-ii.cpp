f4b1c5eba42378ee11904bd05a25c1ff1f3be988
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-09-07 22:51:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (90.96%) - LeetSync

---
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int distinctSubseqII(string s) {
+        const long long MOD = 1e9 + 7;
+
+        long long dp = 1;  // empty subsequence
+
+        vector<long long> last(26, 0);
+
+        for (char c : s) {
+            int x = c - 'a';
+
+            long long newDp = (2 * dp - last[x] + MOD) % MOD;
+
+            last[x] = dp;
+            dp = newDp;
+        }
+
+        // Remove empty subsequence
+        return (dp - 1 + MOD) % MOD;
+    }
+};
