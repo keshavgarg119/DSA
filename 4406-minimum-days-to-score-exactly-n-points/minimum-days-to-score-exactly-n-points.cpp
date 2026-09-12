6f2fb3e959f1e3788d3b3b2cccee47a2da06cedb
keshavgarg119 <kgarg2_be23@thapar.edu>
2026-09-12 22:07:40 +0530

    Time: 271 ms (42.37%) | Memory: 15.6 MB (50.46%) - LeetSync

---
+++ 4406-minimum-days-to-score-exactly-n-points/minimum-days-to-score-exactly-n-points.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minDays(int n) {
+        vector<int>dp(n+1, INT_MAX);
+
+        dp[0] = -1;
+
+        for(int score = 1; score<=n; score++) {
+            for(int k=1; k*(k+1)/2 <= score; k++) {
+
+                int points = k*(k+1)/2;
+
+                if(dp[score-points] != INT_MAX) {
+                    dp[score] = min(dp[score], dp[score-points] + k + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
